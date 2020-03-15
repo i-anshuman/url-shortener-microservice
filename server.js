06d7158b8fcc3350b06d079413a729d25bc8fb64
Anshuman Gupta <anshuman844@gmail.com>
2020-03-15 13:37:15 +0530

    Initail Commit.   - Initialized npm   - Installed express, body-parser, mongodb and mongoose as dependencies   - Installed dotenv and nodemon as dev dependencies   - Added server.js file   - .gitignore node_modules and .env file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+  res.send(`<h1>URL Shortener Microservice.</h1>`);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
